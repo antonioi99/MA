e8b61902b448eec9198871261725c5e2986b4574
a12225670cs <a12225670cs@slurm01.vda.univie.ac.at>
2025-12-26 18:58:36 +0100

    no message

--- main_llm.py
+++ main_llm.py
@@ -8,14 +8,10 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--explanation_format", 
                         type=str,
-                        choices=["baseline", "text_scores", "text_labels", "structured_text_scores", "structured_text_labels", "top_words_scores", "top_words_labels"],  
+                        choices=["baseline", "text_scores", "text_labels", "structured_text_scores", "structured_text_labels", "top_words_scores", "top_words_labels", "natural_words", "part_of_speech"],  
                         required=True)
     parser.add_argument("--data_size",
                         type=int,
-                        required=True)
-    parser.add_argument("--prediction_type",
-                        type=str,
-                        choices=['POSITIVE_NEGATIVE', '0_1'],
                         required=True)
     parser.add_argument("--chain_of_thought",
                         action='store_true',  # Present = True, Absent = False
@@ -30,10 +26,9 @@ def main():
     dev_data_file = "explanations4NLP/merged_data/merged_data.json"
         
     main_folder = 'test_results'
-    prediction_type_folder = f'{args.prediction_type}'
     chain_of_thought_folder = f'chain_of_thought_True' if args.chain_of_thought else 'no_chain_of_thought'
 
-    dir_results = f'{main_folder}/{prediction_type_folder}/{chain_of_thought_folder}'
+    dir_results = f'{main_folder}/{chain_of_thought_folder}'
     os.makedirs(dir_results, exist_ok=True)
 
     if args.start:
@@ -48,7 +43,6 @@ def main():
         dev_data_file=dev_data_file,
         num_test_instances=args.data_size,
         start=args.start,
-        prediction_type=args.prediction_type,
         chain_of_thought=args.chain_of_thought,
         use_explanations=False if args.explanation_format == 'baseline' else True,
         explanation_format=f"{args.explanation_format}",
