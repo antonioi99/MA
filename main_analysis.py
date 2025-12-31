ebab5b5f3c411c87934789278a03b6d5b0dc4a88
a12225670cs <a12225670cs@slurm01.vda.univie.ac.at>
2025-12-31 18:43:16 +0100

    no message

--- main_analysis.py
+++ main_analysis.py
@@ -11,20 +11,26 @@ def main():
 			type=str,
 			choices=["pos_neg", "neg_pos"],
 			required=True)
+    parser.add_argument("--llm",
+                        type=str,
+                        required=True)
+    parser.add_argument("--prompter",
+                        type=str,
+                        required=False)
 
     args = parser.parse_args()
 
-    analysis_folder = 'analysis'
-    chain_of_thought_folder = f'chain_of_thought_True' if args.chain_of_thought else 'no_chain_of_thought'
-    subdirectory = f"{chain_of_thought_folder}/{args.pred_order}"
-    os.makedirs(f"{analysis_folder}/{subdirectory}", exist_ok=True)
+    analysis_main_folder = 'analysis'
+    chain_of_thought_folder = 'chain_of_thought_True' if args.chain_of_thought else 'no_chain_of_thought'
+    subdirectory = f"{args.llm}/{args.prompter}/{chain_of_thought_folder}/{args.pred_order}"
+    os.makedirs(f"{analysis_main_folder}/{subdirectory}", exist_ok=True)
 
     for element in ["baseline", "text_scores", "text_labels", "structured_text_scores", "structured_text_labels", "top_words_scores", "top_words_labels", "natural_words", "part_of_speech"]:
     
         results = helper_analysis.quick_analyze(f'test_results/{subdirectory}/{element}_0_10000.json', 
                                                 'classification_model_predictions/test_set/predictions.json',
                                                 print_results=True,
-                                                save_to=f'{analysis_folder}/{subdirectory}/results_{element}.json')
+                                                save_to=f'{analysis_main_folder}/{subdirectory}/results_{element}.json')
 
 if __name__ == '__main__':
     main()
